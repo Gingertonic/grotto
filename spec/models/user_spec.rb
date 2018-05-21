e450c8dbbc754ab0225102cf97fae127b63f98c2
Gingertonic <bethmschofield@gmail.com>
2018-05-21 14:35:49 +0930

    Create and pass first test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe User do
 
-  it 'has a first name, last name, username and email' 
+  it 'has a first name, last name, username and email' do
+    user = User.new({first_name: "Beth", last_name: "Schofield", username: "Gingertonic", email: "thegingertonicstudios@gmail.com"})
+
+    expect(user).to be_valid
+  end
 
 end
