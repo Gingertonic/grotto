0652ddea39fb61daeeb50893b753f23fd81b72c7
Gingertonic <bethmschofield@gmail.com>
2018-05-21 14:55:55 +0930

    Create and pass first user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  validates :first_name, :last_name, :username, :email, presence: true
+  validates :email, uniqueness: true
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,40 @@ describe User do
     expect(user).to be_valid
   end
 
+  it 'is invalid without a first name' do
+    user = User.create({last_name: "Schofield", username: "Gingertonic", email: "thegingertonicstudios@gmail.com"})
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without a last name' do
+    user = User.create({first_name: "Beth", username: "Gingertonic", email: "thegingertonicstudios@gmail.com"})
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without a username' do
+    user = User.create({first_name: "Beth", last_name: "Schofield", email: "thegingertonicstudios@gmail.com"})
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without an email' do
+    user = User.create({last_name: "Schofield", username: "Gingertonic"})
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid with a duplicate email' do
+    user1 = User.create({first_name: "Beth", last_name: "Schofield", username: "Gingertonic", email: "thegingertonicstudios@gmail.com"})
+    user2 = User.create({first_name: "Beth", last_name: "Schofield", username: "BetiLeti", email: "thegingertonicstudios@gmail.com"})
+
+    expect(user2).to_not be_valid
+  end
+
+  it 'a user knows its full name' do
+    user = User.create({first_name: "Beth", last_name: "Schofield", username: "Gingertonic", email: "thegingertonicstudios@gmail.com"})
+
+    expect(user.full_name).to eq("Beth Schofield")
+  end
 end
